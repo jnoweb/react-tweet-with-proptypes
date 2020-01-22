9821462e0698575f9e80f35cd507f289fe112bea
johnny nolan <johnnynolan@Johnnys-MBP-2.fritz.box>
2020-01-22 17:13:53 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,137 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import moment from 'moment';
+import PropTypes from 'prop-types';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Tweet({ tweet }) {
+    return (
+        <div className='tweet'>
+            <Avatar hash={tweet.gravatar}/>
+            <div className='content'>
+                <Author author={tweet.author}/><Time time={tweet.time}/>
+                <Message text={tweet.message}/>
+                <div className='buttons'>
+                    <ReplyButton/>
+                    <RetweetButton count={tweet.retweets}/>
+                    <LikeButton count={tweet.likes}/>
+                    <MoreOptionsButton/>
+                </div>
+            </div>
+        </div>
+    );
+}
+Tweet.propTypes = {
+    tweet: PropTypes.object.isRequired
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Avatar({ hash }) {
+    const url = `https://www.gravatar.com/avatar/${hash}`;
+    return (
+        <img
+            src={url}
+            className="avatar"
+            alt="avatar"/>
+    );
+}
+Avatar.propTypes = {
+    hash: PropTypes.string.isRequired
+};
+
+function Message({ text }) {
+    return (
+        <div className='message'>
+            { text }
+        </div>
+    );
+}
+Message.propTypes = {
+    text: PropTypes.string
+};
+
+function Author({ author }) {
+    const { name, handle } = author;
+    return (
+        <span className='author'>
+            <span className='name'>{name}</span>
+            <span className='handle'>@{handle}</span>
+        </span>
+    );
+}
+Author.propTypes = {
+    author: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      handle: PropTypes.string.isRequired
+    }).isRequired
+};
+
+const Time = ({ time }) => {
+    const timeString =
+    moment(time).fromNow();
+    return (
+        <span className='time'>
+            {timeString}
+        </span>
+    );
+};
+Time.propTypes = {
+    time: PropTypes.string
+};
+
+const ReplyButton = () => (
+    <i className="fa fa-reply reply-button"/>
+);
+
+function getRetweetCount(count) {
+    if(count > 0) {
+      return (
+        <span className="retweet-count">
+          {count}
+        </span>
+      );
+    } else {
+      return null;
+    }
+  }
+
+const RetweetButton = ({ count }) => (
+    <span className="retweet-button">
+      <i className="fa fa-retweet"/>
+      {getRetweetCount(count)}
+    </span>
+  );
+RetweetButton.propTypes = {
+    count: PropTypes.number
+};
+
+const LikeButton = ({ count }) => (
+    <span className="like-button">
+      <i className="fa fa-heart"/>
+      {count > 0 &&
+        <span className="like-count">
+          {count}
+        </span>}
+    </span>
+);
+LikeButton.propTypes = {
+    count: PropTypes.number
+};
+  
+const MoreOptionsButton = () => (
+    <i className="fa fa-ellipsis-h more-options-button"/>
+);
+
+const testTweet = {
+    message: "This is a tweet using PropTypes.",
+    gravatar: "xyz",
+    author: {
+        handle: "jnoweb",
+        name: "Johnny Nolan"
+    },
+    likes: 2,
+    retweets: 17,
+    timestamp: "2016-07-30 21:24:37"
+};
+
+ReactDOM.render(<Tweet tweet={testTweet}/>,
+    document.querySelector('#root'));
